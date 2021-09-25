33e828c21c3ae732d19b8f5c876fd5479b32e326
David <2018371056@uteq.edu.mx>
2021-09-24 23:28:30 -0500

    Commit inicial

---
+++ App3/App3/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace App3.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ App3/App3/ViewModels/NombreCompletoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace App3.ViewModels
+{
+    public class NombreCompletoViewModel : BaseViewModel
+    {
+        private string nombre;
+        private string app;
+        private string apm;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (string.Equals(nombre, value)) return;
+                nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+        public string ApP
+        {
+            get => app;
+            set
+            {
+                if (string.Equals(app, value)) return;
+                app = value;
+                OnPropertyChanged(nameof(ApP));
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+        public string ApM
+        {
+            get => apm;
+            set
+            {
+                if (string.Equals(apm, value)) return;
+                apm = value;
+                OnPropertyChanged(nameof(ApM));
+                OnPropertyChanged(nameof(nombreCompleto));
+            }
+        }
+
+        public string nombreCompleto => $"{Nombre} {ApP} {ApM}";
+    }
+}
